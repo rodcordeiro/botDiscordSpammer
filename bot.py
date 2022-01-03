959a0e8ad9421df081f4105c2645577bdf753902
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-01-03 10:37:27 -0300

    update bot

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ class DiscordBot:
         while(i<1):
             self.type_like_a_person(self.message,input)
             input.send_keys(Keys.RETURN)
-            # time.sleep(random.randint(1, 5) / 30)
+            time.sleep(random.randint(1, 5) / 30)
     @staticmethod
     def type_like_a_person(sentence, single_input_field):
         """ Este código irá basicamente permitir que você simule a digitação como uma pessoa """
