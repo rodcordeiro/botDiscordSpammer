5283d73fa10d7316415729c4e7d55db588deaf84
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-02-17 13:00:34 -0300

    Adicionado metodo para venda dos pokemons

--- bot.py
+++ bot.py
@@ -11,29 +11,66 @@ class DiscordBot:
         self.driver = profile
         """Instancia do selenium """
         self.message = "iwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhdhqww e woiwej llllll"
-    
+
     def login(self):
         driver = self.driver
         driver.get("https://discord.com/channels/911248622421704704/925735083313348618")
         time.sleep(15)
-        self.spamming()
+        print(self.driver)
+        self.selling()
+
+    def selling(self):
+        driver = self.driver
+        input = driver.find_element(By.XPATH, "//div[@aria-label='Message #spammar']")
+        input.click()
+        for i in range(366):
+            input.send_keys("p")
+            input.send_keys("!")
+            input.send_keys("m")
+            input.send_keys("a")
+            input.send_keys("r")
+            input.send_keys("k")
+            input.send_keys("e")
+            input.send_keys("t")
+            input.send_keys(" ")
+            input.send_keys("a")
+            input.send_keys("d")
+            input.send_keys("d")
+            input.send_keys(" ")
+            for j in str(i).split():
+                input.send_keys(j)
+                time.sleep(1)
+            input.send_keys(" ")
+            input.send_keys("2")
+            input.send_keys("0")
+            input.send_keys(Keys.RETURN)
+            time.sleep(10)
+            try:
+                confirm = driver.find_element(By.XPATH, "//div[@class='label-31sIdr']")
+                confirm.click()
+                time.sleep(2)
+            except:
+                print("Não foi possível localizar o confirm")
+        self.type_like_a_person("p!cleanup", input)
+        self.type_like_a_person("Venda de pokemons concluída", input)
 
     def spamming(self):
         driver = self.driver
-        input = driver.find_element(By.XPATH,"//div[@aria-label='Message #spammar']")
+        input = driver.find_element(By.XPATH, "//div[@aria-label='Message #spammar']")
         input.click()
         i = 0
-        while(i<1):
-            self.type_like_a_person(self.message,input)
+        while i < 1:
+            self.type_like_a_person(self.message, input)
             input.send_keys(Keys.RETURN)
             time.sleep(random.randint(1, 5) / 30)
+
     @staticmethod
     def type_like_a_person(sentence, single_input_field):
-        """ Este código irá basicamente permitir que você simule a digitação como uma pessoa """
+        """Este código irá basicamente permitir que você simule a digitação como uma pessoa"""
         print("going to start typing message into message share text area")
         for letter in sentence.split():
             single_input_field.send_keys(letter)
-            # time.sleep(0.7)
+            time.sleep(0.7)
 
 
 bot = DiscordBot()
