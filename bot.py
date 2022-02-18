e7e8a920799797400ae958394a6bd130308370f5
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-02-18 09:43:28 -0300

    included logging

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import random
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def logging(message):
+    logger.info(message)
+    print(message)
 
 
 class DiscordBot:
     def __init__(self):
         profile = webdriver.Firefox()
-        self.driver = profile
         """Instancia do selenium """
+        self.driver = profile
+        logging("Iniciando browser")
         self.message = "iwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhdhqww e woiwej llllll"
         self.input_label = "Conversar em #spammar"
 
@@ -17,14 +27,16 @@ class DiscordBot:
         driver = self.driver
         driver.get("https://discord.com/channels/911248622421704704/925735083313348618")
         time.sleep(15)
-        self.evolveByCandies(2000)
-        self.spamming()
-        self.changePoke(7)
-        self.evolveByCandies(2000)
-        self.spamming()
+        self.selling(1, 120, 30, False)
+        self.changePoke(114)
+        self.spamming(77)
         self.type_like_a_person("p!cleanup", input)
 
-    def selling(self, start, limit):
+    def selling(self, start=0, limit=300, value=20, reindex=True):
+        logging("Iniciando venda de pokemons")
+        logging(f"ID de inicio {start}")
+        logging(f"ID final {limit}")
+        logging(f"Valor de venda {value}")
         driver = self.driver
         input = driver.find_element(
             By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
@@ -34,7 +46,7 @@ class DiscordBot:
         self.type_like_a_person("p!cleanup", input)
         while i < limit:
             i += 1
-            self.type_like_a_person("p!market add {} 20".format(i), input)
+            self.type_like_a_person("p!market add {} {}".format(i, value), input,True)
             input.send_keys(Keys.RETURN)
             time.sleep(5)
             try:
@@ -43,36 +55,48 @@ class DiscordBot:
                 time.sleep(1)
             except:
                 print("Não foi possível localizar o confirm")
-        self.type_like_a_person("p!reindex", input)
+        self.type_like_a_person("p!reindex", input) if reindex else ""
         self.type_like_a_person("p!cleanup", input)
         self.type_like_a_person("Venda de pokemons concluída", input)
 
-    def spamming(self):
+    def spamming(self, level):
+        logging("Iniciando spamming")
+        logging(f"Spamming para {level} níveis, utilizando {68 * level} mensagens.")
         driver = self.driver
         input = driver.find_element(
             By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
         )
         input.click()
         i = 0
-        while i < 5000:
+        total_messages = 68 * level
+        while i < total_messages:
+            logging(f"Mensagem nº {i}.")
             self.type_like_a_person(self.message, input)
             input.send_keys(Keys.RETURN)
             time.sleep(random.randint(1, 5) / 30)
             i += 1
 
     def evolveByCandies(self, amount):
+        logging(f"Iniciando evolução através de rare candies")
         driver = self.driver
         input = driver.find_element(
             By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
         )
         input.click()
-        while amount > 75:
+        candies = 0
+        balance = amount
+        while balance > 75:
             self.type_like_a_person("p!buy rare candies", input, True)
             input.send_keys(Keys.RETURN)
             time.sleep(2)
-            amount -= 75
+            balance -= 75
+            candies += 1
+        logging(
+            f"Iniciando evolução através de rare candies finalizada, utilizando {candies} candies, com um investimento total de {amount - balance}P¢"
+        )
 
     def changePoke(self, pokeId):
+        logging("Changing selected pokemon")
         driver = self.driver
         input = driver.find_element(
             By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
