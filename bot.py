b97f8e0547904157d1ce1b4563c043e8079f4d1a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-02-18 17:10:17 -0300

    included logger for better information providing

--- bot.py
+++ bot.py
@@ -6,12 +6,13 @@ import time
 import random
 import logging
 
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Spamming bot")
 
 
 def logging(message):
     logger.info(message)
-    print(message)
+    # print(message)
 
 
 class DiscordBot:
@@ -21,14 +22,17 @@ class DiscordBot:
         self.driver = profile
         logging("Iniciando browser")
         self.message = "iwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhdhqww e woiwej llllll"
-        self.input_label = "Conversar em #spammar"
+        self.input_label = "Message #spammar"
 
     def login(self):
         driver = self.driver
         driver.get("https://discord.com/channels/911248622421704704/925735083313348618")
         time.sleep(15)
-        self.selling(1, 120, 30, False)
-        self.changePoke(114)
+        input = driver.find_element(
+            By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
+        )
+        self.type_like_a_person("p!cleanup", input, True)
+        self.selling(1, 130, 20)
         self.spamming(77)
         self.type_like_a_person("p!cleanup", input)
 
@@ -43,20 +47,23 @@ class DiscordBot:
         )
         input.click()
         i = start
-        self.type_like_a_person("p!cleanup", input)
+        self.type_like_a_person("p!cleanup", input, True)
+        time.sleep(10)
         while i < limit:
             i += 1
-            self.type_like_a_person("p!market add {} {}".format(i, value), input,True)
-            input.send_keys(Keys.RETURN)
-            time.sleep(5)
+            time.sleep(10)
+            self.type_like_a_person("p!market add {} {}".format(i, value), input, True)
+            time.sleep(6)
             try:
                 confirm = driver.find_element(By.XPATH, "//div[@class='label-31sIdr']")
                 confirm.click()
                 time.sleep(1)
             except:
                 print("Não foi possível localizar o confirm")
-        self.type_like_a_person("p!reindex", input) if reindex else ""
-        self.type_like_a_person("p!cleanup", input)
+        self.type_like_a_person("p!reindex", input, True) if reindex else ""
+        time.sleep(5)
+        self.type_like_a_person("p!cleanup", input, True)
+        time.sleep(5)
         self.type_like_a_person("Venda de pokemons concluída", input)
 
     def spamming(self, level):
@@ -72,7 +79,6 @@ class DiscordBot:
         while i < total_messages:
             logging(f"Mensagem nº {i}.")
             self.type_like_a_person(self.message, input)
-            input.send_keys(Keys.RETURN)
             time.sleep(random.randint(1, 5) / 30)
             i += 1
 
@@ -87,7 +93,6 @@ class DiscordBot:
         balance = amount
         while balance > 75:
             self.type_like_a_person("p!buy rare candies", input, True)
-            input.send_keys(Keys.RETURN)
             time.sleep(2)
             balance -= 75
             candies += 1
@@ -103,7 +108,6 @@ class DiscordBot:
         )
         input.click()
         self.type_like_a_person("p!s {}".format(pokeId), input, True)
-        input.send_keys(Keys.RETURN)
 
     @staticmethod
     def type_like_a_person(sentence, single_input_field, Timing=False):
@@ -113,6 +117,7 @@ class DiscordBot:
         for letter in text:
             single_input_field.send_keys(letter)
             time.sleep(0.015) if Timing else ""
+        single_input_field.send_keys(Keys.RETURN)
 
 
 bot = DiscordBot()
