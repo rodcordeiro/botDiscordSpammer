163c6c2901a4eeb540ed579b96e7cd48030c6d5a
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-03-24 11:15:05 -0300

    Automated login functionallity

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,22 +17,44 @@ def logging(message):
 
 class Bot:
     def __init__(self):
-        profile = webdriver.Chrome('./drivers/chromedriver_v99.exe')
+        profile = webdriver.Chrome("./drivers/chromedriver_v99.exe")
         """Instancia do selenium """
         self.driver = profile
         logging("Iniciando browser")
         self.message = "iwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhbe wihweiub ewkrjeqiruiqnwe qe qwrioiqje woijdwuii dhqww e woiwej dfhqqoihn ekj rqeeopjdiwqjeqwdb ewjhdhqww e woiwej llllll"
         self.input_label = "Message #spammar"
 
-    def login(self):
+    def login(self, username: str, password: str, has2F: bool = False):
         driver = self.driver
         driver.get("https://discord.com/channels/911248622421704704/925735083313348618")
-        time.sleep(15)
-        input = driver.find_element(
-            By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
-        )
+        time.sleep(5)
+        # <input class="inputDefault-3FGxgL input-2g-os5 inputField-2RZxdl" name="email" type="text" placeholder="" aria-label="E-mail ou número de telefone" autocomplete="off" maxlength="999" spellcheck="false" aria-labelledby="uid_17" value="">
+        input_element = driver.find_element(By.XPATH, "//input[@name='email']")
+        self.type_like_a_person(username, input_element, True)
 
-    def addMarket(self, start: int=0, limit:int=300, value:int=20, reindex:bool=True):
+        # <input class="inputDefault-3FGxgL input-2g-os5" name="password" type="password" placeholder="" aria-label="Senha" autocomplete="off" maxlength="999" spellcheck="false" aria-labelledby="uid_19" value="">
+        input_element = driver.find_element(By.XPATH, "//input[@name='password']")
+        self.type_like_a_person(password, input_element, True)
+        time.sleep(2)
+        try:
+            input_element.send_keys(Keys.RETURN)
+        except:
+            logging("Input not found")
+            pass
+        if has2F is not False:
+            time.sleep(2)
+            # <input class="inputDefault-3FGxgL input-2g-os5" name="" type="text" placeholder="Código de verificação de 6 dígitos/código de backup de 8 dígitos" aria-label="Digite o código de recuperação/autorização do Discord" autocomplete="one-time-code" maxlength="10" spellcheck="false" aria-labelledby="uid_21" value="">
+            input_element = driver.find_element(
+                By.XPATH,
+                "//input[@aria-label='Digite o código de recuperação/autorização do Discord']",
+            )
+            code = input("Please, inform your two factor authentication code: ")
+            self.type_like_a_person(code, input_element, True)
+        time.sleep(2)
+
+    def addMarket(
+        self, start: int = 0, limit: int = 300, value: int = 20, reindex: bool = True
+    ):
         logging("Iniciando venda de pokemons")
         logging(f"ID de inicio {start}")
         logging(f"ID final {limit}")
@@ -56,8 +78,8 @@ class Bot:
         self.type_like_a_person("p!cleanup", input, True)
         time.sleep(5)
         self.type_like_a_person("Venda de pokemons concluída", input)
-    
-    def removeMarket(self, pokes: list[int],reindex:bool=True):
+
+    def removeMarket(self, pokes: list[int], reindex: bool = True):
         logging("Removendo pokemons do mercado")
         logging(f"Total de pokemons a serem removidos: {len(pokes)}")
         driver = self.driver
@@ -78,7 +100,7 @@ class Bot:
         time.sleep(5)
         self.type_like_a_person("Restauração de pokemons concluída", input)
 
-    def spamming(self, level:int):
+    def spamming(self, level: int):
         logging("Iniciando spamming")
         logging(f"Spamming para {level} níveis, utilizando {68 * level} mensagens.")
         driver = self.driver
@@ -94,7 +116,7 @@ class Bot:
             time.sleep(random.randint(1, 5) / 30)
             i += 1
 
-    def evolveByCandies(self, amount:int):
+    def evolveByCandies(self, amount: int):
         logging(f"Iniciando evolução através de rare candies")
         driver = self.driver
         input = driver.find_element(
@@ -111,20 +133,21 @@ class Bot:
         logging(
             f"Iniciando evolução através de rare candies finalizada, utilizando {candies} candies, com um investimento total de {amount - balance}P¢"
         )
-    def buyXpBooster(self, level:int = 1):
-        logging(f"Iniciando evolução através de rare candies")
+
+    def buyXpBooster(self, level: int = 1):
+        logging(f"Iniciando compra de XP Booster {level}")
         driver = self.driver
         input = driver.find_element(
             By.XPATH, "//div[@aria-label='{}']".format(self.input_label)
         )
         input.click()
-        self.type_like_a_person(f"p!buy Xp Booster {level if level > 1 else ''}", input, True)
-        time.sleep(2)
-        logging(
-            f"XP Booster bought."
+        self.type_like_a_person(
+            f"p!buy Xp Booster {level if level > 1 else ''}", input, True
         )
+        time.sleep(2)
+        logging(f"XP Booster bought.")
 
-    def changePoke(self, pokeId:int):
+    def changePoke(self, pokeId: int):
         logging("Changing selected pokemon")
         driver = self.driver
         input = driver.find_element(
@@ -140,15 +163,13 @@ class Bot:
             confirm.click()
             time.sleep(1)
         except:
-            print("Não foi possível localizar o confirm")
-    
+            logging("Não foi possível localizar o confirm")
+
     @staticmethod
     def type_like_a_person(sentence, single_input_field, Timing=False):
         """Este código irá basicamente permitir que você simule a digitação como uma pessoa"""
-        # print("going to start typing message into message share text area")
         text = sentence if Timing else sentence.split()
         for letter in text:
             single_input_field.send_keys(letter)
             time.sleep(0.015) if Timing else ""
         single_input_field.send_keys(Keys.RETURN)
-
