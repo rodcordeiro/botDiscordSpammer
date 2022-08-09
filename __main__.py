73694afd2602a79666a4763e2507d3ca9e5575b6
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-08-09 10:54:11 -0300

    Added voting feature

--- __main__.py
+++ __main__.py
@@ -4,9 +4,10 @@ from decouple import config
 
 bot = Bot()
 bot.login(config("LOGIN"), config("PASSWORD"), has2F=True)
-bot.addMarket(80, 190, 5)
-bot.buyXpBooster(3)
-bot.evolveByCandies(2000)
+bot.vote()
+# bot.addMarket(80, 190, 5)
+# bot.buyXpBooster(3)
+# bot.evolveByCandies(2000)
 # bot.changePoke(5)
 # bot.evolveByCandies(75)
 # bot.spamming(5)
